788abfd8aea947f8bad2260494fbb975efe487be
Fleymus <134969269+Fleymus@users.noreply.github.com>
2024-11-13 19:39:44 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,5 +1,15 @@
-"""This is my file.
-But put here your code please
-instead of this text
-and send me a pull request
-so I will be able to review your code"""
+def Ipower(n, b):
+    if n < 1 or b < 1:
+        return False
+    if n == 1:
+        return True
+    if b == 1:
+        return n == 1
+    power = 1
+    while power < n:
+        power *= b
+    return power == n
+print(Ipower(16,2))
+print(Ipower(12,2))
+print(Ipower(100000, 10))
+print(Ipower(990, 9))
